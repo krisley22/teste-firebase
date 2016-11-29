ab51a7f41e79f250e31eb809026188938364b010
Krisley <krisley.kindermann@jexperts.com.br>
2016-11-29 16:51:05 -0200

    ajuste no controller

--- src/app/waitList/type-spent.controller.js
+++ src/app/waitList/type-spent.controller.js
@@ -12,21 +12,34 @@
 
 
 ]);*/
+(function() {
+  'use strict';
 
+  angular.module('app.typeSpentList', []);
+})();
 
   (function() {
     'use strict';
 
-    angular
-      .module('app.typeSpentList')
+    angular.module('app.typeSpentList')
       .controller('TypeSpentController', TypeSpentController);
 
-    TypeSpentController.$inject = ['cardService', 'user'];
+    TypeSpentController.$inject = ['cardService', 'user', '$scope'];
 
-    function TypeSpentController(cardService, user) {
+    function TypeSpentController(cardService, user, $scope) {
       var vm = this;
 
         vm.typeSpents  = [{'name':'teste'}];
+        $scope.teste = 'testeeeee';
+
+        vm.cards = cardService.getCardsByUser(user.uid);
+
+
+        $scope.add = function addCard() {
+
+          vm.cards.$add(vm.newCard);
+          vm.newCard = new cardService.Card();
+        }
     }
 
   })();
